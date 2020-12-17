3e35fd91cb930558e7e453b05c9009d55eb720dd
Julius Schmidt <25301977+welljsjs@users.noreply.github.com>
2020-12-17 21:57:50 +0100

    Add implementations for string operations
    Add Text_dup, Text_cat, Text_reverse, Text_map, Text_cmp and Text_save

--- src/text.c
+++ src/text.c
@@ -26,6 +26,13 @@
 #define T Text_T
 
 #define idx(i, len) ((i) <= 0 ? (i) + (len) : (i)-1)
+#define isatend(s, n) ((s).str + (s).len == current->avail && current->avail + (n) <= current->limit)
+
+struct Text_save_T
+{
+  struct chunk *current;
+  char *avail;
+};
 
 static char cset[] =
     "\000\001\002\003\004\005\006\007\010\011\012\013\014\015\016\017"
@@ -131,4 +138,150 @@ char *Text_get(char *str, int size, T s)
   memcpy(str, s.str, s.len);
   str[s.len] = '\0';
   return str;
+}
+
+T Text_dup(T s, int n)
+{
+  assert(s.len >= 0 && s.str);
+  assert(n >= 0);
+  if (n == 0 || s.len == 0)
+    return Text_null;
+  if (n == 1)
+    return s;
+  {
+    T text;
+    char *p;
+    text.len = n * s.len;
+    if (isatend(s, text.len - s.len))
+    {
+      text.str = s.str;
+      p = alloc(text.len - s.len);
+      n--;
+    }
+    else
+      text.str = p = alloc(text.len);
+    for (; n-- > 0; p += s.len)
+      memcpy(p, s.str, s.len);
+    return text;
+  }
+}
+
+T Text_cat(T s1, T s2)
+{
+  assert(s1.len >= 0 && s1.str);
+  assert(s2.len >= 0 && s2.str);
+  if (s1.len == 0)
+    return s2;
+  if (s2.len == 0)
+    return s1;
+
+  if (s1.str + s1.len == s2.str)
+  {
+    s1.len += s2.len;
+    return s1;
+  }
+  {
+    T text;
+    text.len = s1.len + s2.len;
+    if (isatend(s1, s2.len))
+    {
+      text.str = s1.str;
+      memcpy(alloc(s2.len), s2.str, s2.len);
+    }
+    else
+    {
+      char *p;
+      text.str = p = alloc(s1.len + s2.len);
+      memcpy(p, s1.str, s1.len);
+      memcpy(p + s1.len, s2.str, s2.len);
+    }
+    return text;
+  }
+}
+
+T Text_reverse(T s)
+{
+  assert(s.len >= 0 && s.str);
+  if (s.len == 0)
+    return Text_null;
+  else if (s.len == 1)
+    return s;
+  else
+  {
+    T text;
+    char *p;
+    int i = s.len;
+    text.len = s.len;
+    text.str = p = alloc(s.len);
+    while (--i >= 0)
+      *p++ = s.str[i];
+    return text;
+  }
+}
+
+T Text_map(T s, T const *from, T const *to)
+{
+  static char map[256];
+  static int inited = 0;
+
+  assert(s.len >= 0 && s.str);
+  if (from && to)
+  {
+    int k;
+    for (k = 0; k < (int)sizeof map; ++k)
+      map[k] = k;
+    assert(from->len == to->len);
+    for (k = 0; k < from->len; ++k)
+      map[(unsigned char)from->str[k]] = to->str[k];
+    inited = 1;
+  }
+  else
+  {
+    assert(from == NULL && to == NULL);
+    assert(inited);
+  }
+  if (s.len == 0)
+    return Text_null;
+  else
+  {
+    T text;
+    int i;
+    char *p;
+    text.len = s.len;
+    text.str = p = alloc(s.len);
+    for (i = 0; i < s.len; ++i)
+      *p++ = map[(unsigned char)s.str[i]];
+    return text;
+  }
+}
+
+int Text_cmp(T s1, T s2)
+{
+  assert(s1.len >= 0 && s1.str);
+  assert(s2.len >= 0 && s2.str);
+  if (s1.str == s2.str)
+    return s1.len - s2.len;
+  else if (s1.len < s2.len)
+  {
+    int cond = memcmp(s1.str, s2.str, s1.len);
+    return cond == 0 ? -1 : cond;
+  }
+  else if (s1.len > s2.len)
+  {
+    int cond = memcmp(s1.str, s2.str, s2.len);
+    return cond == 0 ? +1 : cond;
+  }
+  else
+    return memcmp(s1.str, s2.str, s1.len);
+}
+
+Text_save_T Text_save(void)
+{
+  Text_save_T save;
+
+  NEW(save);
+  save->current = current;
+  save->avail = current->avail;
+  alloc(1);
+  return save;
 }
